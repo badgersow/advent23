d59412e90535c8e03b9d70e90db2fba3b7379402
Efim Pyshnograev <efim@fastmail.com>
2023-12-09 16:50:43 +1100

    Ninth day

---
+++ Ninth.java
@@ -0,0 +1,35 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Ninth {
+    public static void main(String[] args) throws FileNotFoundException {
+        var in = new Scanner(new File("data/ninth.in"));
+
+        long result = 0;
+        while (in.hasNextLine()) {
+            List<Long> initial = Arrays.stream(in.nextLine().split(" ")).map(Long::parseLong).toList();
+            long[][] ext = new long[initial.size() + 1][initial.size() + 1];
+            for (int i = 0; i < initial.size(); i++) {
+                ext[0][i] = initial.get(i);
+            }
+            // Calculate diffs.
+            for (int i = 1; i < ext.length - 1; i++) {
+                for (int j = 0; j < (ext.length - 1) - i; j++) {
+                    ext[i][j] = ext[i - 1][j + 1] - ext[i - 1][j];
+                }
+            }
+            // Extrapolate.
+            for (int i = ext.length - 2; i >= 0; i--) {
+                for (int j = 1; j < (ext.length) - i; j++) {
+                    ext[i][j] = ext[i][j - 1] + ext[i + 1][j - 1];
+                }
+            }
+            result += ext[0][initial.size()];
+        }
+        System.out.println(result);
+    }
+}
